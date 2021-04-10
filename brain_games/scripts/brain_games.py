5fd7b308844e56be41fbf6922b1cee33cd6f6b85
Fessan <fessan@ya.ru>
2021-04-10 12:46:58 +0700

    add

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-def brain_games():
-    print('Welcome to the Brain Games!')
+
+# from brain_games.cli import welcome_user
+
+# import brain_games.cli.welcome_user
+from brain_games.cli import welcome_user
 
 def main():
-    brain_games()
+    welcome_user()
 
 if __name__ == '__main__':
     main()
